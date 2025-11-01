9fb6268681855a3b1f1d7a9309ee3a6b20af5925
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2025-11-02 03:56:19 +0900

    chore: ➕ OpenAPIGenerator, Protobuf, EventStoreを追加

--- Package.swift
+++ Package.swift
@@ -7,15 +7,31 @@ let package = Package(
   name: "swift-sam-vapor-sample",
   platforms: [.macOS(.v26)],
   dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+    .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.0.0"),
+    .package(url: "https://github.com/vapor/swift-openapi-vapor.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0"),
+    .package(url: "https://github.com/lemo-nade-room/event-store-adapter-swift.git", from: "1.0.0"),
+    .package(url: "https://github.com/lemo-nade-room/event-store-adapter-swift-support.git", from: "1.0.0"),
   ],
   targets: [
     .executableTarget(
       name: "App",
       dependencies: [
-        .product(name: "Vapor", package: "vapor")
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
+        .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+        .product(name: "EventStoreAdapter", package: "event-store-adapter-swift"),
+        .product(name: "EventStoreAdapterDynamoDB", package: "event-store-adapter-swift"),
+        .product(name: "EventStoreAdapterSupport", package: "event-store-adapter-swift-support"),
       ],
       swiftSettings: swiftSettings,
+      plugins: [
+        .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+        .plugin(name: "SwiftProtobufPlugin", package: "swift-protobuf"),
+      ],
     ),
     .testTarget(
       name: "AppTests",
